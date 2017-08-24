669e0c5db5fe39f40942fe00980c61e8efb5efe9
Oleg Bondarenko <oleg.bondarenko@mediaux.biz>
2017-08-24 19:54:03 +0300

    Removed notifications, when push message arrives only vibration will be sounded.

--- app/src/main/java/com/geil/myapplication/service/MyFirebaseMessagingService.java
+++ app/src/main/java/com/geil/myapplication/service/MyFirebaseMessagingService.java
@@ -35,20 +35,12 @@ public class MyFirebaseMessagingService extends FirebaseMessagingService {
 
     private static final String TAG = MyFirebaseMessagingService.class.getSimpleName();
 
-    private NotificationUtils notificationUtils;
-
     @Override
     public void onMessageReceived(RemoteMessage remoteMessage) {
         Log.e(TAG, "From: " + remoteMessage.getFrom());
 
         if (remoteMessage == null)
             return;
-
-        // Check if message contains a notification payload.
-        if (remoteMessage.getNotification() != null) {
-            Log.e(TAG, "Notification Body: " + remoteMessage.getNotification().getBody());
-            handleNotification(remoteMessage.getNotification().getBody());
-        }
 
         // Check if message contains a data payload.
         if (remoteMessage.getData().size() > 0) {
@@ -64,40 +56,6 @@ public class MyFirebaseMessagingService extends FirebaseMessagingService {
         }
     }
 
-    private void handleNotification(String message) {
-        if (!NotificationUtils.isAppIsInBackground(getApplicationContext())) {
-            // app is in foreground, broadcast the push message
-            Intent pushNotification = new Intent(Config.PUSH_NOTIFICATION);
-            pushNotification.putExtra("message", message);
-            LocalBroadcastManager.getInstance(this).sendBroadcast(pushNotification);
-            // play notification sound
-            NotificationUtils notificationUtils = new NotificationUtils(getApplicationContext());
-//            notificationUtils.playNotificationSound();
-
-        } else {
-            // If the app is in background, firebase itself handles the notification
-        }
-    }
-
-//    private void doVibrate(String message) {
-//        final Vibrator v = (Vibrator) getSystemService(Context.VIBRATOR_SERVICE);
-//        int count;
-//        if (message.equalsIgnoreCase("message_1")) {
-//            count = 1;
-//        } else if (message.equalsIgnoreCase("message_2")) {
-//            count = 2;
-//        } else {
-//            count = 3;
-//        }
-//        for (int i = 0; i < count; i ++) {
-//            new Handler(Looper.getMainLooper()).postDelayed(new Runnable() {
-//                @Override
-//                public void run() {
-//                    v.vibrate(250);
-//                }
-//            }, 500 * i);
-//        }
-//    }
     private void doVibrate(String message) {
         final Vibrator v = (Vibrator) getSystemService(Context.VIBRATOR_SERVICE);
         int pattern = 0,
@@ -171,49 +129,11 @@ public class MyFirebaseMessagingService extends FirebaseMessagingService {
 //                myRef.child("messages").setValue(uniqueId);
                 myRef.child("messages").child(uniqueId).setValue(msg);
             }
-
-            if (!NotificationUtils.isAppIsInBackground(getApplicationContext())) {
-                // app is in foreground, broadcast the push message
-                Intent pushNotification = new Intent(Config.PUSH_NOTIFICATION);
-                pushNotification.putExtra("message", message);
-                LocalBroadcastManager.getInstance(this).sendBroadcast(pushNotification);
-
-            } else {
-                // app is in background, show the notification in notification tray
-                Intent resultIntent = new Intent(getApplicationContext(), MainActivity.class);
-                resultIntent.putExtra("message", message);
-
-                // check for image attachment
-                if (TextUtils.isEmpty(imageUrl)) {
-                    showNotificationMessage(getApplicationContext(), title, message, timestamp, resultIntent);
-                } else {
-                    // image is present, show notification with image
-                    showNotificationMessageWithBigImage(getApplicationContext(), title, message, timestamp, resultIntent, imageUrl);
-                }
-            }
             doVibrate(message);
         } catch (JSONException e) {
             Log.e(TAG, "Json Exception: " + e.getMessage());
         } catch (Exception e) {
             Log.e(TAG, "Exception: " + e.getMessage());
         }
-    }
-
-    /**
-     * Showing notification with text only
-     */
-    private void showNotificationMessage(Context context, String title, String message, String timeStamp, Intent intent) {
-        notificationUtils = new NotificationUtils(context);
-        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
-        notificationUtils.showNotificationMessage(title, message, timeStamp, intent);
-    }
-
-    /**
-     * Showing notification with text and image
-     */
-    private void showNotificationMessageWithBigImage(Context context, String title, String message, String timeStamp, Intent intent, String imageUrl) {
-        notificationUtils = new NotificationUtils(context);
-        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
-        notificationUtils.showNotificationMessage(title, message, timeStamp, intent, imageUrl);
     }
 }
